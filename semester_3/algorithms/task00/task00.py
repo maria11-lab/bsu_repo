06584dd742b5d4fca4694139fb043bf3ee48fb48
Maria Buchkova <Westmoons7@gmail.com>
2026-09-25 12:22:37 +0300

    add task00.py

--- semester_3/algorithms/task00/task00.py
+++ semester_3/algorithms/task00/task00.py
@@ -1,3 +1,12 @@
-with open ("input.txt") as f:
-    for line in f:
-        numbers = 
+base = set()
+
+with open("input.txt") as inp:
+    for line in inp:
+        base.add(int(line))
+
+s = 0
+for k in base:
+    s += k
+
+with open("output.txt", 'w') as outp:
+    outp.write(str(s))
